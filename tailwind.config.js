ec8e0fc9ab1204370637c85c387bed66bd79093b
yetnx <aymandakir08@gmail.com>
2024-06-29 21:23:14 +0200

    Gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%)',
+      },
+    },
   },
   plugins: [],
 }
