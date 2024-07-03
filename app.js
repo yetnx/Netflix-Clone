cf97dda2c16bf54913f4734ce0f4938a67fa2c75
yetnx <aymandakir08@gmail.com>
2024-07-03 18:27:24 +0200

    Script

---
+++ app.js
@@ -0,0 +1,59 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    var hiddenDiv = document.getElementById('hiddenDiv');
+    if (hiddenDiv.classList.contains('hidden')) {
+      hiddenDiv.classList.remove('hidden');
+    } else {
+      hiddenDiv.classList.add('hidden');
+    }
+  });
+
+
+  document.getElementById('revealButton1').addEventListener('click', function() {
+    var hiddenDiv = document.getElementById('hiddenDiv1');
+    if (hiddenDiv.classList.contains('hidden')) {
+      hiddenDiv.classList.remove('hidden');
+    } else {
+      hiddenDiv.classList.add('hidden');
+    }
+  });
+  
+
+  document.getElementById('revealButton2').addEventListener('click', function() {
+    var hiddenDiv = document.getElementById('hiddenDiv2');
+    if (hiddenDiv.classList.contains('hidden')) {
+      hiddenDiv.classList.remove('hidden');
+    } else {
+      hiddenDiv.classList.add('hidden');
+    }
+  });
+  
+
+  document.getElementById('revealButton3').addEventListener('click', function() {
+    var hiddenDiv = document.getElementById('hiddenDiv3');
+    if (hiddenDiv.classList.contains('hidden')) {
+      hiddenDiv.classList.remove('hidden');
+    } else {
+      hiddenDiv.classList.add('hidden');
+    }
+  });
+  
+
+  document.getElementById('revealButton4').addEventListener('click', function() {
+    var hiddenDiv = document.getElementById('hiddenDiv4');
+    if (hiddenDiv.classList.contains('hidden')) {
+      hiddenDiv.classList.remove('hidden');
+    } else {
+      hiddenDiv.classList.add('hidden');
+    }
+  });
+  
+
+  document.getElementById('revealButton5').addEventListener('click', function() {
+    var hiddenDiv = document.getElementById('hiddenDiv5');
+    if (hiddenDiv.classList.contains('hidden')) {
+      hiddenDiv.classList.remove('hidden');
+    } else {
+      hiddenDiv.classList.add('hidden');
+    }
+  });
+  
