c2470c06825782f615288798beb0e4e4accaadb4
yetnx <aymandakir08@gmail.com>
2024-07-03 19:05:09 +0200

    Update script

--- app.js
+++ app.js
@@ -1,58 +1,39 @@
-document.getElementById('revealButton').addEventListener('click', function() {
-    var hiddenDiv = document.getElementById('hiddenDiv');
-    if (hiddenDiv.classList.contains('hidden')) {
-      hiddenDiv.classList.remove('hidden');
-    } else {
-      hiddenDiv.classList.add('hidden');
-    }
-  });
+  let currentlyVisibleDiv = null;
 
-
-  document.getElementById('revealButton1').addEventListener('click', function() {
-    var hiddenDiv = document.getElementById('hiddenDiv1');
-    if (hiddenDiv.classList.contains('hidden')) {
-      hiddenDiv.classList.remove('hidden');
-    } else {
-      hiddenDiv.classList.add('hidden');
+    function toggleDiv(divId) {
+      const hiddenDiv = document.getElementById(divId);
+      if (currentlyVisibleDiv && currentlyVisibleDiv !== hiddenDiv) {
+        currentlyVisibleDiv.classList.add('hidden');
+      }
+      if (hiddenDiv.classList.contains('hidden')) {
+        hiddenDiv.classList.remove('hidden');
+        currentlyVisibleDiv = hiddenDiv;
+      } else {
+        hiddenDiv.classList.add('hidden');
+        currentlyVisibleDiv = null;
+      }
     }
-  });
-  
 
-  document.getElementById('revealButton2').addEventListener('click', function() {
-    var hiddenDiv = document.getElementById('hiddenDiv2');
-    if (hiddenDiv.classList.contains('hidden')) {
-      hiddenDiv.classList.remove('hidden');
-    } else {
-      hiddenDiv.classList.add('hidden');
-    }
-  });
-  
+    document.getElementById('revealButton').addEventListener('click', function() {
+      toggleDiv('hiddenDiv');
+    });
 
-  document.getElementById('revealButton3').addEventListener('click', function() {
-    var hiddenDiv = document.getElementById('hiddenDiv3');
-    if (hiddenDiv.classList.contains('hidden')) {
-      hiddenDiv.classList.remove('hidden');
-    } else {
-      hiddenDiv.classList.add('hidden');
-    }
-  });
-  
+    document.getElementById('revealButton1').addEventListener('click', function() {
+      toggleDiv('hiddenDiv1');
+    });
 
-  document.getElementById('revealButton4').addEventListener('click', function() {
-    var hiddenDiv = document.getElementById('hiddenDiv4');
-    if (hiddenDiv.classList.contains('hidden')) {
-      hiddenDiv.classList.remove('hidden');
-    } else {
-      hiddenDiv.classList.add('hidden');
-    }
-  });
-  
+    document.getElementById('revealButton2').addEventListener('click', function() {
+      toggleDiv('hiddenDiv2');
+    });
 
-  document.getElementById('revealButton5').addEventListener('click', function() {
-    var hiddenDiv = document.getElementById('hiddenDiv5');
-    if (hiddenDiv.classList.contains('hidden')) {
-      hiddenDiv.classList.remove('hidden');
-    } else {
-      hiddenDiv.classList.add('hidden');
-    }
-  });
+    document.getElementById('revealButton3').addEventListener('click', function() {
+      toggleDiv('hiddenDiv3');
+    });
+
+    document.getElementById('revealButton4').addEventListener('click', function() {
+      toggleDiv('hiddenDiv4');
+    });
+
+    document.getElementById('revealButton5').addEventListener('click', function() {
+      toggleDiv('hiddenDiv5');
+    });
